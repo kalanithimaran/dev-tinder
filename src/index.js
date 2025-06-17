ecd82b57732be82672b8cfcf7040d58b62b21386
kalanithimaran <kalanithimaran88@gmail.com>
2025-06-17 21:08:48 +0530

    multi routers with in array concepts

--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ name: "kalanithimaran" });
-});
-
-app.post("/", (req, res) => {
-  res.send(`User added successfully`);
-});
+app.get(
+  "/students",
+  [(req, res, next) => {
+    next();
+    console.log("first")
+  },[(req, res, next) => {
+    // next();
+    console.log("second")
+    res.send('Hi');
+  }]]
+);
+app.get(
+  "/students",
+  (req, res) => {
+      console.log("second");
+      res.json([
+        { id: 1, name: "Arun" },
+        { id: 2, name: "Divya" },
+      ]);
+    }
+);
 
 app.delete("/", (req, res) => {
   res.send(`User deleted successfully`);
